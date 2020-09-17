c464efac27b4699a4df1067482977d69fa98f40a
Rokan Uddin <rokanu55@gmail.com>
2020-09-17 16:36:35 +0600

    Database connected by mongoose

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db=mongoose.connection;
+db.on('error', (error)=> console.log(error) );
+db.once('open', ()=> console.log("database Connected") );
 
 app.get('/', (req,res,next)=> {
 	res.send("Hello world");
 })
 
 app.listen(3000, ()=> {
-	console.log("Connected ");
+	console.log(" Server Connected ");
 })
